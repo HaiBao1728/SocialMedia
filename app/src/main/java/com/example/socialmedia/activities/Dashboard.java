51df28bbb30f5943528a74b601f5230990f29207
TranBaoTran <117694181+TranBaoTran@users.noreply.github.com>
2024-04-12 13:17:20 +0700

    add dashboard

---
+++ app/src/main/java/com/example/socialmedia/activities/Dashboard.java
@@ -0,0 +1,22 @@
+package com.example.socialmedia.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.socialmedia.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Dashboard extends AppCompatActivity {
+    ActionBar actionBar;
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+        actionBar = getSupportActionBar();
+        bottomNavigationView = findViewById(R.id.navigation);
+    }
+}
