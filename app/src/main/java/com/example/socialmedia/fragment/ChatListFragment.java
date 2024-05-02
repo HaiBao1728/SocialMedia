ff3e0a111114299d953776ace8a79a65b243932f
TranBaoTran <117694181+TranBaoTran@users.noreply.github.com>
2024-05-02 10:30:34 +0700

    add chatlistfragment

--- app/src/main/java/com/example/socialmedia/fragment/ChatListFragment.java
+++ app/src/main/java/com/example/socialmedia/fragment/ChatListFragment.java
@@ -1,15 +1,23 @@
 package com.example.socialmedia.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.socialmedia.R;
+import com.example.socialmedia.activities.MainActivity;
+import com.example.socialmedia.activities.SettingsActivity;
 import com.example.socialmedia.adapters.AdapterChatList;
 import com.example.socialmedia.models.ModelChat;
 import com.example.socialmedia.models.ModelChatList;
@@ -57,6 +65,27 @@ public class ChatListFragment extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerView);
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         userChatList = new ArrayList<>();
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.main_menu, menu);
+                menu.findItem(R.id.add_post).setVisible(false);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.logout) {
+                    mAuth.signOut();
+                    checkUserStatus();
+                    return true;
+                }else if(id==R.id.settings) {
+                    startActivity(new Intent(getActivity(), SettingsActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
         databaseReference = FirebaseDatabase.getInstance().getReference("ChatList").child(currentUser.getUid());
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -76,6 +105,17 @@ public class ChatListFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void checkUserStatus() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            //user is signed in stay here
+            //set email of logged-in user
+        } else {
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        }
     }
 
     private void loadChats() {
